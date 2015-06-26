9b37a415f8bcd99ba7bd57da9ac9f2612d562145
Sudo <key_mtrgtring@yahoo.co.jp>
2015-06-26 12:08:58 +0900

    revise README.md and negaposi.py to read config

--- README.md
+++ README.md
@@ -5,19 +5,32 @@ This program judge negative or positve from twitter.
 
 ##How to use
 
-###Make 'secret.ini'
+###Make configure file, e.g., `secret.ini`
 
-```secret.ini
+```
 [oauth]
 consumer_key = your_consumer_key
 consumer_secret = your_consumer_secret
 
 access_token = your_access_token
 access_token_secret = your_access_token_secret
+  
+[negaposi]
+dict = your_dictionary_path
 ```
 
+###Download dictionary(単語感情極性対応表)
+[http://www.lr.pi.titech.ac.jp/~takamura/pndic_en.html](http://www.lr.pi.titech.ac.jp/~takamura/pndic_en.html)
+
+If you want to judge english tweets, you should download `pn_en.dic`.
+
+If you want to judge japanese tweets, you should download `pn_ja.dic`.
+
+
+
+###How to execute
+
 ```
-$ cd /tmp
 $ git clone https://github.com/daguniko/negaposi.git
 $ cd negaposi
 $ ./negaposi.py
--- negaposi.py
+++ negaposi.py
@@ -13,12 +13,13 @@ consumer_key = conf.get('oauth','consumer_key')
 consumer_secret = conf.get('oauth','consumer_secret')
 access_token = conf.get('oauth','access_token')
 access_token_secret = conf.get('oauth','access_token_secret')
+negaposi_dictionary = conf.get('negaposi','dict')
 
 # setting mecab
 m = MeCab.Tagger("-Ochasen")
 
-# read "単語感情極性対応表"
-f = open('pn_ja.dic').readlines()
+# read dictionary file
+f = open(negaposi_dictionary).readlines()
 dict = {}
 score_list = []
 
@@ -38,26 +39,3 @@ api = tweepy.API(auth)
 your_tweets = api.user_timeline()
 
 print your_tweets
-
-
-#https://apps.twitter.com/app/8368214/keys
-#http://tt-house.com/2013/08/twitter-nega-posi.html
-
-# # for line in your_tweets:
-# # 	print line.text
-# # 	node = m.parseToNode(line.text.encode("utf-8"))
-# # 	while node:
-# # 		if node.feature.split(",")[0] == '形容詞':
-# # 			if dict[node.feature.split(",")[6]]:
-# # 				score_list.append(dict[node.feature.split(",")[6]]["score"].strip())
-
-# # 		node = node.next
-
-
-# # print score_list
-
-# # sum_score = 0.0
-# # for i in score_list:
-# # 	sum_score += float(i)
-# # average_score = sum_score / len(score_list)
-# # print average_score
